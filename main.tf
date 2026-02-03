a8484cb8cd5a73467688f05fbe3dc5914b517e7b
lalithaa2407 <lalitalallu02@gmail.com>
2026-02-03 15:09:13 +0530

    updated module provider version >4.0.0with disabled public access and pe attached uses webapp pvt subnet

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 resource "azurerm_container_app_environment" "example" {
   name                       = "${var.name}-env"
   location                   = var.location
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.log_analytics_workspace.id
   resource_group_name        = var.resource_group_name
   public_network_access = var.public_network_access
   workload_profile {
-    name                  = "Consumption"
-    workload_profile_type = "Consumption"
+    name                  = "${var.name}-envprofile"
+    workload_profile_type = var.env_workload_profile_type
     minimum_count         = 0
     maximum_count         = 10
   }
@@ -16,7 +16,7 @@ resource "azurerm_container_app" "example" {
   container_app_environment_id = azurerm_container_app_environment.example.id
   resource_group_name          =  var.resource_group_name
   revision_mode                = var.revision_mode
-  workload_profile_name        = "Consumption"
+  workload_profile_name        = "${var.name}-cappprofile"
   template {
     container {
       name   = "${var.name}-cnt"
@@ -27,10 +27,37 @@ resource "azurerm_container_app" "example" {
   }
   
 }
-resource "azurerm_log_analytics_workspace" "example" {
-  name                = "${var.name}-logs"
+resource "azurerm_log_analytics_workspace" "log_analytics_workspace" {
+  name                = "${var.name}-log"
   location            = var.location
   resource_group_name = var.resource_group_name
-  sku                 = "PerGB2018"
-  retention_in_days   = 30
+  sku                 = var.sku
+  retention_in_days   = var.retention_in_days
+}
+
+
+# Creates  a private endpoint with private dns
+resource "azurerm_private_endpoint" "endpoint" {
+  name                = "${var.name}-pe"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = var.private_endpoint_subnet_id
+
+  private_service_connection {
+    name                           = "${var.name}-connection"
+    private_connection_resource_id = var.private_connection_resource_id
+    is_manual_connection           = var.is_manual_connection
+    subresource_names              = var.subresource_names
+
+  }
+  private_dns_zone_group {
+    name                 = "${var.name}-dnszone"
+    private_dns_zone_ids = var.private_dns_zone_ids
+  }
+  lifecycle {
+    ignore_changes = [
+      tags,
+    ]
+  }
+
 }
--- variables.tf
+++ variables.tf
@@ -13,6 +13,7 @@ variable "name" {
 variable "revision_mode" {
   type        = string
   description = "os type of the container app"
+  default = "Single"
 }
 variable "image" {
   type        = string
@@ -28,7 +29,79 @@ variable "memory" {
 }
 
 variable "public_network_access" {
-  type = bool
-  default = false
+  type = string
+  default = "Disabled"
+  
+}
+
+
+# private endpoint
+variable "name" {
+  type        = string
+  description = "Specifies the Name of the Private Endpoint. Changing this forces a new resource to be created."
+
+}
+
+variable "private_endpoint_subnet_id" {
+  type        = string
+  description = "The ID of the Subnet from which Private IP Addresses will be allocated for this Private Endpoint."
+
+}
+variable "private_connection_resource_id" {
+  type        = string
+  description = "The ID of the Private Link Enabled Remote Resource which this Private Endpoint should be connected to."
+
+}
+variable "is_manual_connection" {
+  type        = bool
+  description = "Does the Private Endpoint require Manual Approval from the remote resource owner?"
+  default     = false
+
+}
+variable "subresource_names" {
+  type        = list(string)
+  default = [ "managedEnvironment" ]
+  description = " A list of subresource names which the Private Endpoint is able to connect to."
+}
+variable "private_dns_zone_ids" {
+  type        = list(string)
+  description = "Specifies the list of Private DNS Zones to include within the private_dns_zone_group."
+}
+
+variable "ip_configuration" {
+  type        = bool
+  default     = false
+  description = "One or more ip_configuration blocks as defined below. This allows a static IP address to be set for this Private Endpoint, otherwise an address is dynamically allocated from the Subnet."
+}
+
+
+
+
+
+variable "sku" {
+  type        = string
+  description = "Specifies the SKU of the Log Analytics Workspace. Possible values are Free, PerNode, Premium, Standard, Standalone, Unlimited, CapacityReservation, and PerGB2018 (new SKU as of 2018-04-03). Defaults to PerGB2018."
+}
+
+variable "retention_in_days" {
+  type        = number
+  description = "The workspace data retention in days. Possible values are either 7 (Free Tier only) or range between 30 and 730."
+  default     = 30
+}
+
+variable "env_workload_profile_type" {
+  type = string
+  default = "Consumption"
+  
+}
+
+variable "minimum_count" {
+  type = number
+  default = 0
+  
+}
+variable "maximum_count" {
+  type = number
+  default = 10
   
 }
